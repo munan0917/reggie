4df846e7520e9552c91cf2d41ecbc3e969215e8b
zqn <zqn0917@163.com>
2022-09-20 15:33:52 +0800

    RestController

--- reggie_take_out/src/main/java/com/itheima/reggie/common/helloGit.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/common/helloGit.java
@@ -1,4 +1,8 @@
 package com.itheima.reggie.common;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class helloGit {
 }
